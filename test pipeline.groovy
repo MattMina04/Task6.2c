aca78403d5034ea4ad71b4f0e4fa8a3e5375bec8
MattMina04 <51625253+MattMina04@users.noreply.github.com>
2023-05-12 21:28:33 +1000

    Update test pipeline.groovy

--- test pipeline.groovy
+++ test pipeline.groovy
@@ -4,58 +4,56 @@ pipeline {
   stages {
     stage('Build') {
       steps {
-        echo 'Build stage: Compiling and packaging the code'
-        // Example: Use Maven as the build automation tool
-        // sh 'mvn clean package'
+        // Use Maven as the build automation tool
+        sh 'mvn clean package'
       }
     }
     
     stage('Unit and Integration Tests') {
       steps {
-        echo 'Unit and Integration Tests stage: Running unit tests and integration tests'
-        // Example: Use JUnit for unit tests and Selenium for integration tests
-        // sh 'mvn test'
-        // sh 'mvn integration-test'
+        // Use JUnit for unit tests and Selenium for integration tests
+        sh 'mvn test'
+        sh 'mvn integration-test'
       }
     }
     
     stage('Code Analysis') {
       steps {
-        echo 'Code Analysis stage: Analyzing the code for industry standards'
-        // Example: Use Jenkins plugins like Checkstyle, FindBugs, or SonarQube for code analysis
-        // sh 'mvn checkstyle:checkstyle'
+        // Use Jenkins plugins like Checkstyle, FindBugs, or SonarQube to analyze the code
+        // Example: Checkstyle
+        sh 'mvn checkstyle:checkstyle'
       }
     }
     
     stage('Security Scan') {
       steps {
-        echo 'Security Scan stage: Performing a security scan on the code'
-        // Example: Use a security scanning tool like OWASP ZAP or SonarQube
-        // sh 'zap-cli --quick-scan --spider <target-url>'
+        // Use a security scanning tool like OWASP ZAP or SonarQube for security analysis
+        // Example: OWASP ZAP
+        sh 'zap-cli --quick-scan --spider <target-url>'
       }
     }
     
     stage('Deploy to Staging') {
       steps {
-        echo 'Deploy to Staging stage: Deploying the application to a staging server'
-        // Example: Use a deployment tool (e.g., AWS CLI) to deploy to the staging environment
-        // sh 'aws ec2 deploy-to-staging'
+        // Use a deployment tool (e.g., AWS CLI) to deploy to the staging environment
+        // Example: AWS CLI
+        sh 'aws ec2 deploy-to-staging'
       }
     }
     
     stage('Integration Tests on Staging') {
       steps {
-        echo 'Integration Tests on Staging stage: Running integration tests on the staging environment'
-        // Example: Use Selenium for integration tests
-        // sh 'mvn integration-test -Denvironment=staging'
+        // Run integration tests on the staging environment
+        // Example: Selenium
+        sh 'mvn integration-test -Denvironment=staging'
       }
     }
     
     stage('Deploy to Production') {
       steps {
-        echo 'Deploy to Production stage: Deploying the application to a production server'
-        // Example: Use a deployment tool (e.g., AWS CLI) to deploy to the production environment
-        // sh 'aws ec2 deploy-to-production'
+        // Use a deployment tool (e.g., AWS CLI) to deploy to the production environment
+        // Example: AWS CLI
+        sh 'aws ec2 deploy-to-production'
       }
     }
   }
@@ -67,27 +65,23 @@ pipeline {
     }
     
     success {
-      echo 'Pipeline Success: Sending success notification email'
-      // Send success notification email with status and log attachments
-      // Example: Use the Jenkins email-ext plugin
-      // emailext (
-      //   to: 'your-email@example.com',
-      //   subject: 'Pipeline Success',
-      //   body: 'The pipeline ran successfully. Logs are attached.',
-      //   attachmentsPattern: 'logs/**'
-      // )
+      // Send success notification email
+      emailext (
+        to: 'your-email@example.com',
+        subject: 'Pipeline Success',
+        body: 'The pipeline ran successfully. Logs are attached.',
+        attachmentsPattern: 'logs/**'
+      )
     }
     
     failure {
-      echo 'Pipeline Failure: Sending failure notification email'
-      // Send failure notification email with status and log attachments
-      // Example: Use the Jenkins email-ext plugin
-      // emailext (
-      //   to: 'your-email@example.com',
-      //   subject: 'Pipeline Failure',
-      //   body: 'The pipeline failed. Logs are attached.',
-      //   attachmentsPattern: 'logs/**'
-      // )
+      // Send failure notification email
+      emailext (
+        to: 'your-email@example.com',
+        subject: 'Pipeline Failure',
+        body: 'The pipeline failed. Logs are attached.',
+        attachmentsPattern: 'logs/**'
+      )
     }
   }
 }
