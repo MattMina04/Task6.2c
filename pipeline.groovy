8e4eb2d1f86e7e4835a2080ea58bebfbf552e314
MattMina04 <51625253+MattMina04@users.noreply.github.com>
2023-05-12 21:48:02 +1000

    Update pipeline.groovy

--- pipeline.groovy
+++ pipeline.groovy
@@ -4,50 +4,59 @@ pipeline {
   stages {
     stage('Build') {
       steps {
-        echo 'Build stage: Compiling and packaging the code'
-        echo 'You can use a build automation tool like Maven here.'
+        echo 'Build stage: Compiling and packaging the code using Maven'
+        // Use Maven to build the code
+        // Example: sh 'mvn clean install'
       }
     }
     
     stage('Unit and Integration Tests') {
       steps {
         echo 'Unit and Integration Tests stage: Running unit tests and integration tests'
-        echo 'You can use test automation tools like JUnit and Selenium here.'
+        // Use test automation tools like JUnit and Selenium for tests
+        // Example: sh 'mvn test'
       }
     }
     
     stage('Code Analysis') {
       steps {
-        echo 'Code Analysis stage: Analyzing the code for industry standards'
-        echo 'You can use a code analysis tool like Checkstyle, FindBugs, or SonarQube integrated with Jenkins here.'
+        echo 'Code Analysis stage: Analyzing the code using a code analysis tool integrated with Jenkins'
+        // Use a code analysis tool integrated with Jenkins (e.g., SonarQube)
+        // Example: withSonarQubeEnv('SonarQube Server') {
+        //              sh 'mvn sonar:sonar'
+        //          }
       }
     }
     
     stage('Security Scan') {
       steps {
-        echo 'Security Scan stage: Performing a security scan on the code'
-        echo 'You can use a security scanning tool like OWASP ZAP or SonarQube here.'
+        echo 'Security Scan stage: Performing a security scan on the code using a security scanning tool'
+        // Use a security scanning tool (e.g., OWASP ZAP)
+        // Example: sh 'zap-cli --scan <path_to_code>'
       }
     }
     
     stage('Deploy to Staging') {
       steps {
-        echo 'Deploy to Staging stage: Deploying the application to a staging server'
-        echo 'You can use deployment tools like AWS CLI or Ansible here.'
+        echo 'Deploy to Staging stage: Deploying the application to a staging server (e.g., AWS EC2 instance)'
+        // Use deployment tools like AWS CLI or Ansible to deploy to staging
+        // Example: sh 'aws s3 cp <artifact_path> s3://<staging_bucket>'
       }
     }
     
     stage('Integration Tests on Staging') {
       steps {
         echo 'Integration Tests on Staging stage: Running integration tests on the staging environment'
-        echo 'You can use test automation tools like Selenium here.'
+        // Use test automation tools like Selenium for integration tests
+        // Example: sh 'mvn integration-test'
       }
     }
     
     stage('Deploy to Production') {
       steps {
-        echo 'Deploy to Production stage: Deploying the application to a production server'
-        echo 'You can use deployment tools like AWS CLI or Ansible here.'
+        echo 'Deploy to Production stage: Deploying the application to a production server (e.g., AWS EC2 instance)'
+        // Use deployment tools like AWS CLI or Ansible to deploy to production
+        // Example: sh 'aws s3 cp <artifact_path> s3://<production_bucket>'
       }
     }
   }
@@ -61,13 +70,23 @@ pipeline {
     success {
       echo 'Pipeline Success: Sending success notification email'
       // Send success notification email with status and log attachments
-      echo 'You can use a notification plugin like email-ext to send email notifications.'
+      // Example: emailext (
+      //              subject: 'Pipeline Success',
+      //              body: 'The pipeline has successfully completed.',
+      //              to: 'email@example.com',
+      //              attachmentsPattern: 'logs/**'
+      //          )
     }
     
     failure {
       echo 'Pipeline Failure: Sending failure notification email'
       // Send failure notification email with status and log attachments
-      echo 'You can use a notification plugin like email-ext to send email notifications.'
+      // Example: emailext (
+      //              subject: 'Pipeline Failure',
+      //              body: 'The pipeline has failed. Please check the logs for details.',
+      //              to: 'email@example.com',
+      //              attachmentsPattern: 'logs/**'
+      //          )
     }
   }
 }
