a0bce762eaed0d2564691a61e6e590e195d097fa
MattMina04 <51625253+MattMina04@users.noreply.github.com>
2023-05-12 21:59:53 +1000

    Update pipeline.groovy

--- pipeline.groovy
+++ pipeline.groovy
@@ -1,94 +1,71 @@
-pipeline {
-  agent any
-  
-  stages {
-    stage('Build') {
-      steps {
-        echo 'Build stage: Compiling and packaging the code using Maven'
-        // Use Maven to build the code
-        // Example: sh 'mvn clean install'
-      }
+pipeline{
+    agent any
+    stages{
+        stage('Build') {
+            steps {
+                echo 'Building the code using Maven'
+            }
+        }
+        stage('Unit and Integration Tests') {
+            steps {
+                echo 'Running unit and integration tests using JUnit and Selenium'
+            }
+            post{
+                always{
+                    emailext (
+                        subject: 'Unit and Integration Tests Status',
+                        to: 'madhikarmianshu@gmail.com',
+                        body: "${currentBuild.result}: Job ",
+                        attachLog: true
+                    )
+                }
+            }
+        }
+        stage('Code Analysis') {
+            steps {
+                echo 'Analyzing the code using Jenkins and SonarQube'
+            }
+        }
+        stage('Security Scan') {
+            steps {
+                echo 'Performing a security scan on the code using OWASP ZAP'
+            }
+            post{
+                always{
+                    emailext (
+                        subject: 'Security Scan Status',
+                        to: 'madhikarmianshu@gmail.com',
+                        body: "${currentBuild.result}: Job ",
+                        attachLog: true,
+                    )
+                }
+            }
+        }
+        stage('Deploy to Staging') {
+            steps {
+                echo 'Deploying the application to an AWS EC2 instance'
+            }
+        }
+        stage('Integration Tests on Staging') {
+            steps {
+                echo 'Running integration tests on the staging environment using Selenium'
+            }
+            post{
+                always{
+                    emailext (
+                        subject: 'Integration Tests on Staging Status',
+                        to: 'madhikarmianshu@gmail.com',
+                        body: "${currentBuild.result}: Job ",
+                        attachLog: true,
+                    )
+                }
+            }
+        }
+        stage('Deploy to Production') {
+            steps {
+                echo 'Deploying the application to an AWS EC2 instance'
+            }
+        }
     }
-    
-    stage('Unit and Integration Tests') {
-      steps {
-        echo 'Unit and Integration Tests stage: Running unit tests and integration tests'
-        // Use test automation tools like JUnit and Selenium for tests
-        // Example: sh 'mvn test'
-      }
-    }
-    
-    stage('Code Analysis') {
-      steps {
-        echo 'Code Analysis stage: Analyzing the code using a code analysis tool integrated with Jenkins'
-        // Use a code analysis tool integrated with Jenkins (e.g., SonarQube)
-        // Example: withSonarQubeEnv('SonarQube Server') {
-        //              sh 'mvn sonar:sonar'
-        //          }
-      }
-    }
-    
-    stage('Security Scan') {
-      steps {
-        echo 'Security Scan stage: Performing a security scan on the code using a security scanning tool'
-        // Use a security scanning tool (e.g., OWASP ZAP)
-        // Example: sh 'zap-cli --scan <path_to_code>'
-      }
-    }
-    
-    stage('Deploy to Staging') {
-      steps {
-        echo 'Deploy to Staging stage: Deploying the application to a staging server (e.g., AWS EC2 instance)'
-        // Use deployment tools like AWS CLI or Ansible to deploy to staging
-        // Example: sh 'aws s3 cp <artifact_path> s3://<staging_bucket>'
-      }
-    }
-    
-    stage('Integration Tests on Staging') {
-      steps {
-        echo 'Integration Tests on Staging stage: Running integration tests on the staging environment'
-        // Use test automation tools like Selenium for integration tests
-        // Example: sh 'mvn integration-test'
-      }
-    }
-    
-    stage('Deploy to Production') {
-      steps {
-        echo 'Deploy to Production stage: Deploying the application to a production server (e.g., AWS EC2 instance)'
-        // Use deployment tools like AWS CLI or Ansible to deploy to production
-        // Example: sh 'aws s3 cp <artifact_path> s3://<production_bucket>'
-      }
-    }
-  }
-  
-  post {
-    always {
-      // Archive logs as attachments
-      archiveArtifacts artifacts: 'logs/**', fingerprint: true
-    }
-  }
-  
-  post {
-    success {
-      echo 'Pipeline Success: Sending success notification email'
-      // Send success notification email with status and log attachments
-      // Example: emailext (
-      //              subject: 'Pipeline Success',
-      //              body: 'The pipeline has successfully completed.',
-      //              to: 'email@example.com',
-      //              attachmentsPattern: 'logs/**'
-      //          )
-    }
-    
-    failure {
-      echo 'Pipeline Failure: Sending failure notification email'
-      // Send failure notification email with status and log attachments
-      // Example: emailext (
-      //              subject: 'Pipeline Failure',
-      //              body: 'The pipeline has failed. Please check the logs for details.',
-      //              to: 'email@example.com',
-      //              attachmentsPattern: 'logs/**'
-      //          )
-    }
-  }
+
 }
